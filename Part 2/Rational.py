67b5d2b2c9a0227f4769ea0d411f2efbe8e19a83
AntoninaDavidenko <44519188+AntoninaDavidenko@users.noreply.github.com>
2022-12-13 20:18:10 +0200

    Update Rational.py

--- Part 2/Rational.py
+++ Part 2/Rational.py
@@ -1,20 +1,20 @@
 class Rational:
 
     def __init__(self):
-        self.numerator = 1
-        self.denominator = 2
+        self.__numerator = 1
+        self.__denominator = 2
 
     def set_numerator(self, numerator):
-        self.numerator = numerator
+        self.__numerator = numerator
 
     def set_denominator(self, denominator):
-        self.denominator = denominator
+        self.__denominator = denominator
 
     def standard_form(self):
-        print(f"{self.numerator}/{self.denominator}")
+        print(f"{self.__numerator}/{self.__denominator}")
 
     def floating_point(self):
-        result = self.numerator / self.denominator
+        result = self.__numerator / self.__denominator
         print(result)
 
 
